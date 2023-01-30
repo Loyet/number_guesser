3dd2e1633a7ced889d7fd65e24719f14b1367555
Mike Loyet <mikeloyet@gmail.com>
2023-01-30 04:39:58 -0600

    Add check to see if entry is valid

--- number_guesser.py
+++ number_guesser.py
@@ -1,6 +1,18 @@
 import random
 
-# Ask user to enter a number
+# Ask user to enter a number.
 top_number = input("Type a number: ")
+
+#Make sure entry is a valid number.
+if top_number.isdigit():
+    top_number = int(top_number)
+
+    #If number is less than 0, tell the user so and exit the program.
+    if top_number <= 0:
+        print('Please type a number larger than 0 next time.')
+        quit()
+else:
+    print('Please type a number next time.')
+    quit()
 
 random_number = random.randint(-1, 11)
